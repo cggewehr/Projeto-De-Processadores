7de030c88b6d9de50f81298e2a050863f3b878dd
Carlos <cggewehr@gmail.com>
2019-04-25 22:22:59 -0300

    VHDL do T2 funcionando (bidirecional)

--- T3P1/NovaPortaBidirecional.vhd
+++ T3P1/NovaPortaBidirecional.vhd
@@ -48,6 +48,8 @@ entity BidirectionalPort is
         
         -- Processor interface
         data        : inout std_logic_vector (DATA_WIDTH-1 downto 0);
+        --data_in     : in std_logic_vector (DATA_WIDTH-1 downto 0);
+        --data_out    : out std_logic_vector (DATA_WIDTH-1 downto 0);
         address     : in std_logic_vector (1 downto 0);         -- NÃO ALTERAR!
         rw          : in std_logic; -- 0: read; 1: write
         ce          : in std_logic;
@@ -68,105 +70,136 @@ architecture Behavioral of BidirectionalPort  is
     signal TRISTATE_DATA_TEMP : std_logic_vector (DATA_WIDTH-1 downto 0); -- Sinal temporario para comunicação com processador
     signal TRISTATE_DATA_EN   : std_logic;
     
-    signal TRISTATE_PORT_IO_TEMP : std_logic_vector (DATA_WIDTH-1 downto 0);
+    --signal TRISTATE_PORT_IO_TEMP : std_logic_vector (DATA_WIDTH-1 downto 0);
     signal TRISTATE_PORT_IO_EN   : std_logic_vector (DATA_WIDTH-1 downto 0);
     
 begin
  
-    -- ESCRITA NOS REGISTRADORES
-    process(rst, clk) begin
-        if(rst = '1') then
-            -- Reseta registradores
-            REG_PORT_DATA <= (others=>'0');
-            REG_PORT_CONFIG <= (others=>'0');
-            REG_PORT_ENABLE <= (others=>'0');
-            
-        elsif rising_edge(clk) then
-            -- PORT DATA           
-            if address = PORT_DATA_ADDR then
-                for i in 0 to DATA_WIDTH-1 loop
---                    if TRISTATE_PORT_IO_EN(i) = '1' then
---                        REG_PORT_DATA(i) <= data(i);
---                    elsif TRISTATE_PORT_IO_EN(i) = '0' then
+--    -- ESCRITA NOS REGISTRADORES
+--    process(rst, clk) begin
+--        if(rst = '1') then
+--            -- Reseta registradores
+--            REG_PORT_DATA <= (others=>'0');
+--            REG_PORT_CONFIG <= (others=>'1'); -- Entrada
+--            REG_PORT_ENABLE <= (others=>'0');
+--            
+--        elsif rising_edge(clk) then
+--            -- PORT DATA           
+--            if address = PORT_DATA_ADDR then
+--                for i in 0 to DATA_WIDTH-1 loop
+----                    if TRISTATE_PORT_IO_EN(i) = '1' then
+----                        REG_PORT_DATA(i) <= data(i);
+----                    elsif TRISTATE_PORT_IO_EN(i) = '0' then
+----                        REG_PORT_DATA(i) <= REG_SYNC2(i);
+----                    end if;
+--                    if REG_PORT_CONFIG(i) = '0' then -- SAIDA
+--                        if ce = '1' then
+--                            REG_PORT_DATA(i) <= data(i);
+--                       -- else
+--                       --     null;
+--                        end if;
+--                    else -- REG_PORT_CONFIG(i) = '1'
 --                        REG_PORT_DATA(i) <= REG_SYNC2(i);
 --                    end if;
-                    if REG_PORT_CONFIG(i) = '0' then -- SAIDA
-                        if ce = '1' then
-                            REG_PORT_DATA(i) <= data(i);
-                        else
-                            null;
-                        end if;
-                    else -- REG_PORT_CONFIG(i) = '1'
-                        REG_PORT_DATA(i) <= REG_SYNC2(i);
-                    end if;
-                end loop; 
-            end if;     
-        
-            -- PORT CONFIG (0 = saida, 1 = entrada)
-            if address = PORT_CONFIG_ADDR then
-                if ( ce = '1' and rw = '1') then
-                    REG_PORT_CONFIG <= data;
-                end if;
-            end if;
+--                end loop; 
+--            end if;     
+--        
+--            -- PORT CONFIG (0 = saida, 1 = entrada)
+--            if address = PORT_CONFIG_ADDR then
+--                if ( ce = '1' and rw = '1') then
+--                    REG_PORT_CONFIG <= data;
+--                end if;
+--            end if;
+--            
+--            -- PORT ENABLE
+--            if address = PORT_ENABLE_ADDR then
+--                if ( ce = '1' and rw = '1') then
+--                    REG_PORT_ENABLE <= data;
+--                end if; 
+--            end if;
+--        end if; 
+--    end process;
+	 
+    -- REGISTRADOR DE DADOS
+	process(clk, rst) begin
+        if rst = '1' then
+			REG_PORT_DATA <= (others=>'0');
+		elsif rising_edge(clk) then
+			for i in 0 to DATA_WIDTH-1 loop
             
-            -- PORT ENABLE
-            if address = PORT_ENABLE_ADDR then
-                if ( ce = '1' and rw = '1') then
-                    REG_PORT_ENABLE <= data;
-                end if; 
+                -- Saida / PROCESSADOR -> PORTA
+				if TRISTATE_PORT_IO_EN(i) = '1' and address = PORT_DATA_ADDR and ce = '1' and rw = '1' then
+					--REG_PORT_DATA(i) <= data_in(i);
+					REG_PORT_DATA(i) <= data(i);
+				end if;
+                    
+                --Entrada / EXTERIOR -> PORTA
+                if REG_PORT_CONFIG(i) = '1' and REG_PORT_ENABLE(i) = '1' then
+                    REG_PORT_DATA(i) <= REG_SYNC2(i);
+                end if;
+			end loop;
+		end if;
+	 end process;
+	 
+	-- REGISTRADOR DE CONFIGURAÇÂO
+    process(clk, rst) begin
+        if rst = '1' then
+            REG_PORT_CONFIG <= (others=>'1'); -- Default como entrada
+        elsif rising_edge(clk) then
+            if address = PORT_CONFIG_ADDR and ce = '1' and rw = '1' then
+                --REG_PORT_CONFIG <= data_in;
+                REG_PORT_CONFIG <= data;
             end if;
-        end if; 
+        end if;
+    end process;
+    
+    -- REGISTRADOR DE ENABLE
+    process(clk, rst) begin
+        if rst = '1' then
+            REG_PORT_ENABLE <= (others=>'0');
+        elsif rising_edge(clk) then
+            if address = PORT_ENABLE_ADDR and ce = '1' and rw = '1' then
+                --REG_PORT_ENABLE <= data_in;
+                REG_PORT_ENABLE <= data;
+            end if;
+        end if;
     end process;
     
     -- REGISTRADORES DE SINCRONIZACAO
-    process(clk, rst) begin
+    REG_SYNC: process(clk, rst) begin
         if rst = '1' then
             REG_SYNC1 <= (others=>'0');
+            REG_SYNC2 <= (others=>'0');
         elsif rising_edge(clk) then
             REG_SYNC1 <= port_io;
-        end if;
-    end process;
-    
-    process(clk, rst) begin
-        if rst = '1' then
-            REG_SYNC2 <= (others=>'0');
-        elsif falling_edge(clk) then
             REG_SYNC2 <= REG_SYNC1;
         end if;
-    end process;  
+    end process; 
     
-    -- SINAL DE LEITURA PARA PROCESSADOR
+    -- MULTIPLEXADOR PARA TRISTATE_DATA
     TRISTATE_DATA_TEMP <= REG_PORT_DATA when address = PORT_DATA_ADDR else
                           REG_PORT_CONFIG when address = PORT_CONFIG_ADDR else
-                          REG_PORT_ENABLE when address = PORT_ENABLE_ADDR else
-                          (others=>'0');
+                          REG_PORT_ENABLE;
     
     -- CONTROLE TRISTATE PARA PROCESSADOR
     TRISTATE_DATA_EN <= '1' when rw = '0' and ce = '1' else '0';
 
     -- TRISTATE PARA PROCESSADOR
     data <= TRISTATE_DATA_TEMP when TRISTATE_DATA_EN = '1' else (others=>'Z'); -- read
+
+    -- BARRAMENTO DE SAIDA
+--    data_out <= REG_PORT_DATA when address = PORT_DATA_ADDR else
+--                REG_PORT_CONFIG when address = PORT_CONFIG_ADDR else
+--                REG_PORT_ENABLE;
     
-    -- CONTROLE TRISTATE PARA CONEXAO DA PORTA E MULTIPLEXADOR DE REG_PORT_DATA
-    process(REG_PORT_DATA, REG_PORT_CONFIG, REG_PORT_ENABLE) begin
-        for i in 0 to (DATA_WIDTH-1) loop
-            if REG_PORT_CONFIG(i) = '0' and REG_PORT_ENABLE(i) = '1' then
-                TRISTATE_PORT_IO_EN(i) <= '1';
-            else
-                TRISTATE_PORT_IO_EN(i) <= '0';
-            end if;     
-        end loop;
-    end process;
+    -- CONTROLE TRISTATE PARA CONEXAO DA PORTA
+    TRISTATE_SAIDA_EN: for i in 0 to DATA_WIDTH-1 generate
+        TRISTATE_PORT_IO_EN(i) <= '1' when REG_PORT_CONFIG(i) = '0' and REG_PORT_ENABLE(i) = '1' else '0';
+    end generate;
     
     -- TRISTATE PARA CONEXAO DA PORTA
-    process(REG_PORT_DATA, REG_PORT_CONFIG, REG_PORT_ENABLE) begin
-        for i in 0 to DATA_WIDTH-1 loop
-            if TRISTATE_PORT_IO_EN(i) = '1' then
-                port_io(i) <= REG_PORT_DATA(i);
-            else
-                port_io(i) <= 'Z';
-            end if;
-        end loop;
-    end process;
+    TRISTATE_SAIDA: for i in 0 to DATA_WIDTH-1 generate
+        port_io(i) <= REG_PORT_DATA(i) when TRISTATE_PORT_IO_EN(i) = '1' else 'Z';
+    end generate;
     
 end architecture;
--- T3P1/r8_uC.vhd
+++ T3P1/r8_uC.vhd
@@ -18,12 +18,13 @@ end R8_uC;
 
 architecture behavioral of R8_uC is
    
-    signal clk_2                                 : std_logic;                      -- 50 MHz clock
-	signal reset_sync                            : std_logic;                      -- Synchronized reset
-	signal ce, rw                                : std_logic;                      -- Auxiliary signals for R8 processor instantiation
-	signal rw_MEM, clk_MEM, en_MEM, en_PORTA     : std_logic;                      -- Auxiliary signals for R8 processor instantiation
-    signal data_PORTA, data_MEM_in, data_mem_out : std_logic_vector(15 downto 0); 
-    signal data_in_r8, data_out_r8, address      : std_logic_vector(15 downto 0);
+    signal clk_2                                     : std_logic;                      -- 50 MHz clock
+	signal reset_sync                                : std_logic;                      -- Synchronized reset
+	signal ce, rw                                    : std_logic;                      -- Auxiliary signals for R8 processor instantiation
+	signal rw_MEM, clk_MEM, en_MEM, en_PORTA         : std_logic;                      -- Auxiliary signals for R8 processor instantiation
+    --signal data_PORTA_out, data_MEM_in, data_mem_out : std_logic_vector(15 downto 0); 
+    signal data_PORTA, data_MEM_in, data_mem_out     : std_logic_vector(15 downto 0); 
+    signal data_r8_in, data_r8_out, address          : std_logic_vector(15 downto 0);
 
     alias address_PORTA                    : std_logic_vector(1 downto 0) is address(1 downto 0);
     alias mem_address                      : std_logic_vector(14 downto 0)is address(14 downto 0);
@@ -31,18 +32,26 @@ architecture behavioral of R8_uC is
     alias REG_PERIFERICO                   : std_logic_vector(3 downto 0) is address(3 downto  0);  -- Registador do periferico
     alias ENABLE_PERIFERICO                : std_logic is address(15);  -- Enable do periferico
 	
-	signal TRISTATE_TO_PORT   : std_logic_vector(15 downto 0);
+	--signal TRISTATE_TO_PORT   : std_logic_vector(15 downto 0);
 	signal TRISTATE_TO_EN     : std_logic;
    
 begin
-	
-    data_in_r8 <= data_MEM_out when ENABLE_PERIFERICO = '0' else data_PORTA;
+    
+    -- Sinais da Porta
+    en_PORTA <= '1' when (ce = '1' and ENABLE_PERIFERICO = '1') else '0';
+    -- Sinais do Processador
+    --data_r8_in <= data_MEM_out when ENABLE_PERIFERICO = '0' else data_PORTA_out;
+    data_r8_in <= data_MEM_out when ENABLE_PERIFERICO = '0' else data_PORTA;
+    -- Sinais da Memoria 
+    clk_MEM <= not clk_2; -- seta o clock da memoria em clock complementar
+    rw_MEM  <= not rw;    -- escrita e leitura para borda complementar
+    en_MEM  <= '1' when (ce = '1' and ENABLE_PERIFERICO = '0') else '0'; -- address(15)   
     
 	-- TRISTATE PARA PORTA
-	TRISTATE_TO_PORT <= data_out_r8 when TRISTATE_TO_EN = '1' else (others=>'Z');
+	data_PORTA <= data_r8_out when TRISTATE_TO_EN = '1' else (others=>'Z');
 	TRISTATE_TO_EN <= '1' when rw = '0' else '0';  -- Enables when writes
     
-    data_PORTA <= TRISTATE_TO_PORT;
+   -- data_PORTA <= TRISTATE_TO_PORT;
 
     ClockManager: entity work.ClockManager
         port map(
@@ -57,42 +66,36 @@ begin
             rst_in  => rst,
             rst_out => reset_sync
         );
-        
+         
     -- Processador
     R8Processor: entity work.R8 
         port map(
             clk      => clk_2,
             rst      => reset_sync,
             address  => address,
-            data_out => data_out_r8,
-            data_in  => data_in_r8,
+            data_out => data_r8_out,
+            data_in  => data_r8_in,
             ce       => ce,
             rw       => rw
         );
         
-    -- Sinais da Memoria 
-    clk_MEM <= not clk_2; -- seta o clock da memoria em clock complementar
-    rw_MEM  <= not rw;    -- escrita e leitura para borda complementar
-    en_MEM  <= '1' when (ce = '1' and ENABLE_PERIFERICO = '0') else '0'; -- address(15)
     
     -- Memoria
     Memory: entity work.Memory 
         generic map(
             DATA_WIDTH => 16,
             ADDR_WIDTH => 15,
-            IMAGE => "AssemblyT3P1_BRAM.txt" -- Arquivo assembly
+            IMAGE => "PortaIO_BRAM.txt" -- Arquivo assembly
         )
         port map(
             clk => clk_MEM,
             wr  => rw_MEM,
             en  => en_MEM,
             address  => mem_address, 
-            data_in  => data_out_r8,
+            data_in  => data_r8_out,
             data_out => data_MEM_out    
         );
         
-    -- Sinais da Porta
-    en_PORTA <= '1' when (ce = '1' and ENABLE_PERIFERICO = '1') else '0';
         
     -- Porta de entrada e saida    
     PORTA: entity work.BidirectionalPort
@@ -103,10 +106,12 @@ begin
 			PORT_ENABLE_ADDR    => "10"
         )
         port map(
-            clk => clk_MEM, 
+            clk => clk_2, 
             rst => reset_sync,
 				
             -- Processor Interface
+            --data_in => data_R8_out,
+            --data_out => data_PORTA_out,
             data => data_PORTA,
             address => address_PORTA,
             rw => rw_MEM,              -- 0: read; 1: write
