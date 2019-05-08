c97e267f927c54dec838711ab6c931d8a17418f8
Carlos <cggewehr@gmail.com>
2019-05-08 17:13:14 -0300

    Comentarios

--- T4P1/r8_uC.vhd
+++ T4P1/r8_uC.vhd
@@ -1,7 +1,8 @@
--------------------------------------------------------------------------
+-------------------------------------------------------------------------------------------------------------
 -- Design unit: R8_uC
--- Description: 
--------------------------------------------------------------------------
+-- Description: Instantiation of R8 processor with an I/O Port, meant for synthisys on Nexys 3 board
+-- Author: Carlos Gewehr and Emilio Ferreira (cggewehr@gmail.com, emilio.ferreira@ecomp.ufsm.br)
+------------------------------------------------------------------------------------------------------------
 
 library IEEE;
 use IEEE.std_logic_1164.all;
@@ -18,22 +19,21 @@ end R8_uC;
 
 architecture behavioral of R8_uC is
    
-    signal clk_2                                     : std_logic;                      -- 50 MHz clock
+    signal clk_2                                     : std_logic;                      -- 50 MHz clock from DCM
 	signal reset_sync                                : std_logic;                      -- Synchronized reset
 	signal ce, rw                                    : std_logic;                      -- Auxiliary signals for R8 processor instantiation
-	signal rw_MEM, clk_MEM, en_MEM, en_PORTA         : std_logic;                      -- Auxiliary signals for R8 processor instantiation
-    --signal data_PORTA_out, data_MEM_in, data_mem_out : std_logic_vector(15 downto 0); 
-    signal data_PORTA, data_MEM_in, data_mem_out     : std_logic_vector(15 downto 0); 
+	signal rw_MEM, clk_MEM, en_MEM, en_PORT          : std_logic;                      -- Auxiliary signals for R8 processor instantiation
+
+    signal data_PORT, data_MEM_in, data_mem_out      : std_logic_vector(15 downto 0); 
     signal data_r8_in, data_r8_out, address          : std_logic_vector(15 downto 0);
 
-    alias address_PORTA                    : std_logic_vector(1 downto 0) is address(1 downto 0);
+    alias address_PORT                     : std_logic_vector(1 downto 0) is address(1 downto 0);
     alias mem_address                      : std_logic_vector(14 downto 0)is address(14 downto 0);
-    alias ID_PERIFERICO                    : std_logic_vector(3 downto 0) is address(7 downto  4);  -- Id do periferico
-    alias REG_PERIFERICO                   : std_logic_vector(3 downto 0) is address(3 downto  0);  -- Registador do periferico
-    alias ENABLE_PERIFERICO                : std_logic is address(15);  -- Enable do periferico
+    alias ID_PERIFERICO                    : std_logic_vector(3 downto 0) is address(7 downto  4);  -- Perf. ID
+    alias REG_PERIFERICO                   : std_logic_vector(3 downto 0) is address(3 downto  0);  -- Perf. Address
+    alias ENABLE_PERIFERICO                : std_logic is address(15); 							    -- Perf. Enable (I/O operation to be carried out on peripheral)
 	
-	-- Tristate between data_out (from R8) and port
-	--signal TRISTATE_TO_PORT   : std_logic_vector(15 downto 0);
+    -- Tristate for bidirectional bus between processor and i/o port
 	signal TRISTATE_TO_EN     : std_logic;
 	
 	-- Interruption Interface
@@ -42,10 +42,7 @@ architecture behavioral of R8_uC is
    
 begin
 
-	-- TRISTATE PARA PORTA
-	data_PORTA <= data_r8_out when TRISTATE_TO_EN = '1' else (others=>'Z');
-	TRISTATE_TO_EN <= '1' when rw = '0' else '0';  -- Enables when writes
-
+	-- Xilinx DCM
     ClockManager: entity work.ClockManager
         port map(
             clk_in   => clk,
@@ -53,6 +50,7 @@ begin
             clk_div4 => open
         );
         
+    -- Reset synchronizer    
     ResetSynchronizer: entity work.ResetSynchronizer
         port map(
             clk     => clk_2,
@@ -60,13 +58,18 @@ begin
             rst_out => reset_sync
         );
 		
-    -- Sinais do Processador
-    data_r8_in <= data_MEM_out when ENABLE_PERIFERICO = '0' else data_PORTA;
-	
-    -- Processador
+    -- Processor signals
+    data_r8_in <= data_MEM_out when ENABLE_PERIFERICO = '0' else data_PORT;
+
+    -- Peripheral generated interruption
+    irq_R8 <= ( irq_PORT(15) or irq_PORT(14) or irq_PORT(13) or irq_PORT(12) or irq_PORT(11) or irq_PORT(10) or irq_PORT(9) or irq_PORT(8) or
+                irq_PORT(7) or irq_PORT(6) or irq_PORT(5) or irq_PORT(4) or irq_PORT(3) or irq_PORT(2) or irq_PORT(1) or irq_PORT(0) ); 
+    --irq_R8 <= or irq_PORT -- Only works if compiled on VHDL-2008
+
+    -- Processor
     R8Processor: entity work.R8 
 		generic map(
-			ISR_ADDR => "0000000000000001" -- Endereço de ISR
+			ISR_ADDR => "0000000000000001" -- Address for ISR, always @ second memory position
 		)
         port map(
             clk      => clk_2,
@@ -76,20 +79,20 @@ begin
             data_out => data_r8_out,
             data_in  => data_r8_in,
             ce       => ce,
-            rw       => rw
+            rw       => rw                 -- Write : 0, Read : 1
         );
 		
-    -- Sinais da Memoria 
-    clk_MEM <= not clk_2; -- seta o clock da memoria em clock complementar
-    rw_MEM  <= not rw;    -- escrita e leitura para borda complementar
+    -- Memory signals
+    clk_MEM <= not clk_2; -- Makes memory sensitive to falling edge
+    rw_MEM  <= not rw;    -- Writes when 0, Reads when 1
     en_MEM  <= '1' when (ce = '1' and ENABLE_PERIFERICO = '0') else '0'; -- address(15)      
     
-    -- Memoria
+    -- Memory
     Memory: entity work.Memory 
         generic map(
             DATA_WIDTH => 16,
             ADDR_WIDTH => 15,
-            IMAGE => "AssemblyT3P2_BRAM.txt" -- Arquivo assembly
+            IMAGE => "NovoAssemblyT3P2_BRAM.txt" -- Assembly code (must be in same directory)
         )
         port map(
             clk => clk_MEM,
@@ -100,16 +103,15 @@ begin
             data_out => data_MEM_out    
         );
 		
-    -- Sinais da Porta
-    en_PORTA <= '1' when (ce = '1' and ENABLE_PERIFERICO = '1') else '0';   
-	
-	irq_R8 <= (irq_PORT(15) or irq_PORT(14) or irq_PORT(13) or irq_PORT(12) or 
-				  irq_PORT(11) or irq_PORT(10) or irq_PORT(9)  or irq_PORT(8)  or 
-				  irq_PORT(7)  or irq_PORT(6)  or irq_PORT(5)  or irq_PORT(4)  or 
-				  irq_PORT(3)  or irq_PORT(2)  or irq_PORT(1)  or irq_PORT(0) );
-				  
-    -- Porta de entrada e saida    
-    PORTA: entity work.BidirectionalPort
+    -- Port signals
+    en_PORT <= '1' when (ce = '1' and ENABLE_PERIFERICO = '1') else '0';   
+        
+    -- Tristate between i/o port and processor
+	data_PORT <= data_r8_out when TRISTATE_TO_EN = '1' else (others=>'Z');
+	TRISTATE_TO_EN <= '1' when rw = '0' else '0';  -- Enables when writes
+
+    -- I/O port 
+    IO_Port: entity work.BidirectionalPort
         generic map(
 			DATA_WIDTH          => 16, -- Port width in bits
 			PORT_DATA_ADDR      => "00",    
@@ -122,10 +124,10 @@ begin
             rst => reset_sync,
 				
             -- Processor Interface
-            data => data_PORTA,
-            address => address_PORTA,
+            data => data_PORT,
+            address => address_PORT,
             rw => rw_MEM,              -- 0: read; 1: write
-            ce => en_PORTA,
+            ce => en_PORT,
 			irq => irq_PORT,
 				
             -- External interface
