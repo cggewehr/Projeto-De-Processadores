d707fbc1d9cead0fe3ff5df6077c5daf5f3a2748
Carlos <cggewehr@gmail.com>
2019-06-06 18:05:30 -0300

    tb

--- T5P2/r8_uc_tb.vhd
+++ T5P2/r8_uc_tb.vhd
@@ -9,17 +9,35 @@ architecture behavioral of r8_uC_tb is
     signal clk : std_logic := '0';
     signal rst : std_logic;
     signal port_io : std_logic_vector(15 downto 0);
+    signal uart_rx_data_out : std_logic_vector(7 downto 0);
+    signal uart_rx_data_av : std_logic;
 	 
 begin
     R8: entity work.R8_uC_TOPLVL
         port map(
-            clk => clk,
-            rst => rst
+            clock => clk,
+            reset => rst,
+            port_io_uC => port_io,
+            uart_tx => uart_tx
         );
 
     clk <= not clk after 5 ns; -- 100 MHz
     rst <= '1', '0' after 5 ns;
-	 port_io <= "ZZZZZZZZZZZZZZZZ";
+	port_io <= "ZZZZZZZZZZZZZZZZ";
 
-    --end process;
+
+    -- UART RX
+    UART_RX: entity work.UART_RX
+        generic map(
+            RATE_FREQ_BAUD => 10416
+        )
+
+        port map(
+            clk => clk,
+            rst => rst,
+            rx => uart_tx,
+            data_out => uart_rx_data_out,
+            data_av => uart_rx_data_av
+        );
+
 end behavioral;
