3d35c08df2ae5f933c11be41d70aaaf974e8efbc
Carlos <cggewehr@gmail.com>
2019-06-10 14:57:31 -0300

    baud certo

--- T5P2/r8_uC.vhd
+++ T5P2/r8_uC.vhd
@@ -171,8 +171,8 @@ begin
 
     TX: entity work.UART_TX
         generic map(
-            --RATE_FREQ_BAUD => 5208 -- 9600 baud @ 50MHz
-            RATE_FREQ_BAUD => 4 -- Simulation
+            RATE_FREQ_BAUD => 5208 -- 9600 baud @ 50MHz
+            --RATE_FREQ_BAUD => 4 -- Simulation
         )
         port map (
             clk     => clk,
