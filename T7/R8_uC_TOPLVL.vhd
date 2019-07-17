4e74f748ff121d20eee32a16a16d9c9613cea78e
Carlos <cggewehr@gmail.com>
2019-07-17 09:10:42 -0300

    wip

--- T7/R8_uC_TOPLVL.vhd
+++ T7/R8_uC_TOPLVL.vhd
@@ -45,7 +45,7 @@ begin
     -- R8 Microcontroller (R8 Processor, Memory, I/O Port, PIC, UART TX and RX)
     Microcontroller: entity work.R8_uC
     	generic map (
-    	    RAM_IMAGE     => "AssemblyT7_somente_display_BRAM.txt",
+    	    RAM_IMAGE     => "AssemblyT7_display1ms_BRAM.txt",
     	    ROM_IMAGE     => "ROM_IMAGE_BRAM.txt",
             ADDR_PORT     => "0000",
             ADDR_PIC      => "1111",
